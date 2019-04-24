97997d007d672da2ead7b973e660558e84bab976
chatbotlbminh <chatbotlbminh@gmail.com>
2019-04-24 22:03:19 +0700

    'first'

---
+++ main.py
@@ -0,0 +1,29 @@
+# import pdb
+# pdb.set_trace()
+# import os
+# import time
+#time.sleep(180)
+#os.system('pulseaudio -D')
+#time.sleep(1)
+#os.system('sudo pacmd set-source-volume 1 200000')
+#time.sleep(1)
+#os.system('sudo pulseaudio --start')
+#time.sleep(1)
+
+
+
+import dem
+import threading
+import execute
+
+
+
+
+
+
+
+
+execute.run_thread(dem.mainloop)
+# execute.run_thread(gu.showGUI)
+# execute.run_thread(gu.background)
+# execute.run_thread(gu.key_watch)
